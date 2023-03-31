d2d962b5bbc0fe464b6c2f5d58e31fc6496f143c
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2023-03-31 12:09:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ print("########################## Running Test FIle in server ##############")
 
 from subprocess import check_output, STDOUT
 cmd = "mkdir Testing_Folder"
-cmd1 = "echo "Text_Content" >> ./Testing_Folder"
+cmd1 = "echo \"Text_Content\" >> ./Testing_Folder/test.txt"
 check_output(cmd, shell=True, stderr=STDOUT, universal_newlines=True)
 check_output(cmd1, shell=True, stderr=STDOUT, universal_newlines=True)
 
