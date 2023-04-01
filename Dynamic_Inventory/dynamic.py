4e5e908fd38f4fbe337faeabd8e1f0c077c12a47
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2023-04-01 17:50:15 +0530

    Delete Dynamic_Inventory directory

--- Dynamic_Inventory/dynamic.py
+++
@@ -1,9 +0,0 @@
-print("########################## Running Test FIle in server - Dynamic Inventory ##############")
-
-from subprocess import check_output, STDOUT
-cmd = "mkdir Dynamic_Folder"
-cmd1 = "echo \"Text_Content\" >> ./Dynamic_Folder/test.txt"
-check_output(cmd, shell=True, stderr=STDOUT, universal_newlines=True)
-check_output(cmd1, shell=True, stderr=STDOUT, universal_newlines=True)
-
-print("########################## End Folder ##############################")
