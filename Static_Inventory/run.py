5155e1927d3a47beae6f3f28ed37087907a24fed
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2023-04-01 12:14:59 +0530

    Update run.py

--- Static_Inventory/run.py
+++ Static_Inventory/run.py
@@ -4,7 +4,7 @@ print("########################## Running Test FIle in server ##############")
 
 from subprocess import check_output, STDOUT
 cmd = "mkdir Static_Folder"
-cmd1 = "echo \"Static_Inventory\" >> ./Testing_Folder/test.txt"
+cmd1 = "echo \"Static_Inventory\" >> ./Static_Folder/test.txt"
 check_output(cmd, shell=True, stderr=STDOUT, universal_newlines=True)
 check_output(cmd1, shell=True, stderr=STDOUT, universal_newlines=True)
 
